b7d3782218da9401bb1e8dd6b2c395832e9b66e5
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:30:47 +0530

    age calculation

---
+++ task1.js
@@ -0,0 +1,12 @@
+// var dob_year="2000";
+// var year="2025";
+// var age = year-dob_year;
+// var age2 = age--;
+// console.log("I will be either " +age2 +" or " +age +" in " +year);
+// var age=23;
+// var max=80;
+// var estamt=200;
+// var rst_yr=max-age;
+// var eatrstlf=(200*(rst_yr*365));
+// console.log("You will need " +eatrstlf +" ruppees to last you until the ripe old age of " +max);
+
