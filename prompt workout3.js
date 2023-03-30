5f715734252eddd6de9c4af339aa841b53d38089
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:56:38 +0530

    prompt workout3

---
+++ prompt workout3.js
@@ -0,0 +1,7 @@
+var name=prompt("Enter your name");
+var age=prompt("Enter your age");
+var c=365*age;
+var max=80*365;
+var balance=max-c;
+alert(name+ " you are in this world for "+c +" days");
+alert("you will be alive  in this world for "+balance +"days");
