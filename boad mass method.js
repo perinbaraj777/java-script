8b91982f9fba52e8e74f8a866a85708851ebea34
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:45:52 +0530

    boad mass method

---
+++ boad mass method.js
@@ -0,0 +1,42 @@
+let a="1000",b=222,c=30;
+
+console.log(a);
+let x="helo";
+let y="world";
+x=(a===1000);
+console.log(x);
+console.log(x+="earth");
+let s="name";
+console.log(typeof(s));
+let a="1000",b=222,c=30;
+
+console.log(a);
+let x="helo";
+let y="world";
+x=(a===1000);
+console.log(x);
+console.log(x+="earth");
+let s="name";
+console.log(typeof(s));
+{
+     <p id="demo"></p>
+const a=3;
+const b=4;
+let y=a+b;
+document.getElementById('demo').innerHTML="The value of y is:"+z; }
+
+var a ="surya ", b="s";
+x=a+b;
+console.log(x);
+var a ="surya"+" "+"s";{
+console.log(a); <p id="demo"></p>
+const a=3;
+const b=4;
+let y=a+b;
+document.getElementById('demo').innerHTML="The value of y is:"+z; }
+
+var a ="surya ", b="s";
+// x=a+b;
+// console.log(x);
+var a="surya"+" "+"s";
+console.log(a);
