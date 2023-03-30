cf73fd5034f89eca9e1ef230871c0f32658876ef
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:38:00 +0530

    functions created

---
+++ functions.js
@@ -0,0 +1,28 @@
+function a(){
+        console.log(data);
+    }
+    function b(){
+        console.log(data); 
+    }
+    
+    function c(){
+        var data=400;
+        console.log(data);
+    }
+    function d(){
+        var data=500;
+        console.log(data);
+    }
+    a();
+    b();
+    c();
+    d();
+    function e(){
+    let x="welcome home";
+    console.log(x);
+    }
+    e();
+    {
+        var name="surya";
+        console.log(name);
+    }
