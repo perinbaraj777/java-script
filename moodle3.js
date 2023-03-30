2ad285c0d0ed683da68cfaa9db90240e80fbb824
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:55:22 +0530

    moodle 3

---
+++ moodle3.js
@@ -0,0 +1,13 @@
+var weekdays=["mon","tue","wed","thr","fri"];
+console.log(weekdays[3]);
+console.log(weekdays.length);
+console.log(weekdays[0]);
+document.writeln(weekdays.length +"<br>");
+document.write(weekdays[3] +"<br>");
+document.write(typeof weekdays);
+function msg(){
+    alert("hello this a message");
+    
+}
+
+
