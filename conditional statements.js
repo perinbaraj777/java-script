946c85d401e8101754ebecaeb918ddcbadd176c9
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:59:07 +0530

    conditional statements

---
+++ conditional statements.js
@@ -0,0 +1,15 @@
+let mark=prompt("Entermark");
+if(mark>=50 && mark<100)
+{
+    document.write("congratulation you have passed");
+}
+else if (mark>100)
+{
+    document.write("Enter correct value below 100 ")
+}
+else
+{
+    document.write("Go study well you are failed");
+}
+
+
