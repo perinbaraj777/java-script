b4166e18a9ef70338c97702bae3ad49f13c722ef
perinbaraj777 <perinbaraj777@gmail.com>
2023-03-30 22:49:47 +0530

    moodle.js created

---
+++ moodle.js
@@ -0,0 +1,12 @@
+var a=5;
+var b=7;
+var e=13;
+var x=((b+e)+10);
+ var c=a*b;
+ var d=13/7;
+window.alert(c);
+window.alert(d);
+document.write(e+=b);
+document.write("<br>");
+document.write(x);
+
