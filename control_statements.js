2b95623953bdf92147323f9f896d909e896c2585
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-04 16:42:00 +0530

    do while implemented

---
+++ control_statements.js
@@ -0,0 +1,34 @@
+// for loop statements
+// for(var i=0;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+// for checking even numbers
+// for(var i=0;i<=10;i++){
+// if(i%2!==0)
+// console.log(i);
+
+// }
+
+// // for checking even numbers without i++
+// for( var i=0;i<=10;i+=2){
+//     if(i%2==0)
+//     console.log(i);    
+//     }
+
+
+// // for while loop
+// let i=11;
+// while(i<=15){
+//     console.log(i);
+//     i++;
+// }
+  
+
+// do-while
+let i=21;
+do{
+    console.log(i);
+    i++;
+}while(i<=25);
