c74097994f87ff4e5e934f2938262e17b0a63ca5
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-04 16:40:41 +0530

    get element by id is  used

--- function.js
+++ function.js
@@ -1,9 +1,17 @@
-function add1()
-{
-    let a=10;
-    let b=10;
-    let c=a+b;
-    console.log(c);
-}
-add1();
-var data=
+// function add1()
+// {
+//     let a=10;
+//     let b=10;
+//     let c=a+b;
+//     console.log(c);
+// }
+// add1();
+// var data=
+function add(){
+    var a=document.getElementById("inp1").innerHTML;
+    var b=document.getElementById("inp2").innerHTML;
+    var c=a+b;
+    //document.getElementById("res")=c;
+    document.write(c);
+    }
+    add();
