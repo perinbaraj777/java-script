031b186c0e567d6576dd9e19b92991c92378eef9
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-04 16:00:48 +0530

    executed math objects functions

---
+++ math.js
@@ -0,0 +1,11 @@
+let a=Math.random(0,1);
+console.log(a);
+console.log(Math.sqrt(a));
+console.log(Math.abs(-10));
+console.log(Math.cbrt(8));
+console.log(Math.floor(2.5));
+console.log(Math.floor(2.1));
+console.log(Math.ceil(8.9))
+console.log(Math.pow(2,3));
+console.log(Math.round(8.5));
+console.log(Math.PI);
