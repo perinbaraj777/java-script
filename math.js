85889fb25f371a8c3c4699e0b0ff7d90482a0f53
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-04 16:38:51 +0530

    finding area of circle

--- math.js
+++ math.js
@@ -1,11 +1,18 @@
-let a=Math.random(0,1);
-console.log(a);
-console.log(Math.sqrt(a));
-console.log(Math.abs(-10));
-console.log(Math.cbrt(8));
-console.log(Math.floor(2.5));
-console.log(Math.floor(2.1));
-console.log(Math.ceil(8.9))
-console.log(Math.pow(2,3));
-console.log(Math.round(8.5));
-console.log(Math.PI);
+// let a=Math.random(0,1);
+// console.log(a);
+// console.log(Math.sqrt(a));
+// console.log(Math.abs(-10));
+// console.log(Math.cbrt(8));
+// console.log(Math.floor(2.5));
+// console.log(Math.floor(2.1));
+// console.log(Math.ceil(8.9))
+// console.log(Math.pow(2,3));
+// console.log(Math.round(8.5));
+// console.log(Math.PI);
+
+// finding area of circle
+let a=Math.PI;
+let r= 5;
+let radius=Math.pow(r,2);
+let area=(a*radius);
+console.log(area);
