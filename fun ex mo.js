847090a5cbfe4aa36dda9ffd582274fd9b1a3694
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-07 17:21:39 +0530

    finding largest number

---
+++ fun ex mo.js
@@ -0,0 +1,13 @@
+let num1=20;
+    let num2=15;
+    let num3=10;
+    if(num1>num2&&num3){
+      console.log(num1+ " is largest");
+
+    }
+    else if(num2>num3){
+      console.log(num2+" is largest");
+    }
+    else{
+      console.log(num3);
+    }
