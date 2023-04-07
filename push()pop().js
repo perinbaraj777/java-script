8bfaccd135342304c185dac8d4f9acef2abe2836
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-07 15:54:38 +0530

    performing push()pop() function dynamically

---
+++ push()pop().js
@@ -0,0 +1,12 @@
+
+
+var n=[""];  
+function push(){
+        var a=document.getElementById("one").value;
+    n.push(a);
+   var c=document.getElementById("ary").innerHTML=n+"<br>";
+    // document.write(n);
+    //document.write(n);
+    console.log(n);
+
+}
