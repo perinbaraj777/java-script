830bb59e64f51e4aaaa11cf1b2bfdd2c3a61dfa4
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-09 14:32:36 +0530

    Advanced  objects operations executed

---
+++ Advancedobject.js
@@ -0,0 +1,85 @@
+
+                            //  ADVANCED OBJECTS OPERATIONS
+
+                                                  // to  change  an property value inside an object 
+
+
+// let person={
+//   firstname:"surya",
+//   lastname:"s",
+//   age:32
+// }
+// person.age=15;               //replaces  the value as 15
+// console.log(person);
+
+
+
+       //to add an new property and value inside an object
+
+// let person={
+//     firstname:"surya",
+//     lastname:"s",
+//     age:32
+//   }
+//   person.gender="male";   //adds the new property and value to the object
+//   console.log(person);
+  
+  
+  
+
+
+                           //function as a value
+                                  
+                                  //  is used to declare a function inside an object 
+
+// let person={
+//   firstname:"surya",
+//   lastname:"s",
+//   age:32,
+//   run:function(){
+//     let a=10;
+//     let b=10;
+//     let c=a+b;
+//     console.log(c);
+
+//   }
+  
+// }
+// person.run()
+// console.log(person);
+
+
+
+
+
+                                    // ARRAY AS A VALUE
+                                          // to declare an array inside
+// let sports={
+//   hockey:"national game",
+//   circket:"famous game",
+//   swiming:"intresting game",
+//   habbit:["money","passion"]
+
+// }
+// console.log(sports.habbit);
+// console.log(sports);
+// console.log(sports["habbit"][1])
+
+                                      
+
+                                                          //OBJECT AS A VALUE
+
+                                                          //to declare an object inside an object
+
+
+// let sports={
+//   hockey:"national game",
+//   circket:"famous game",
+//   swiming:"intresting game",
+//   habbit:{
+//     money:"a lot of expense",
+//    condition :"passion"}
+// }
+// console.log(`${sports.hockey}  yuy ${ sports.habbit.condition  }`);     //back tick and dollarsymbol is used to add two conditions without + symbol
+// console.log(sports.habbit.condition)                                     //prints passion 
+
