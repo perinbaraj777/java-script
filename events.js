50a6003efd8550ddde7bbad6b8cec09efd679dba
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-09 21:46:45 +0530

    button, mouse ,keyboard,form, windows events are executed

---
+++ events.js
@@ -0,0 +1,76 @@
+//  function msg(){
+//     alert("enter your name");
+//  }
+
+
+                                                               // MOUSE OVER EVENT
+
+// function msg(){
+
+    // alert("enter your name");        used to alert the output while mouse hover by calling the function
+
+//    document.getElementById("mouse").innerHTML="ramkumar";       // used to get the output on the p tag while mouse placed on the button
+
+//   }
+  
+
+
+                                        // <!-- KEYBOARD EVENT -->
+
+                                                                   // <!-- onkeydown -->                             
+// function msg(){
+//   alert("enter your name");
+//  }
+
+
+
+
+
+                            //  FORM EVENTS
+                                          
+                                        // onfocus 
+                                           
+                                                   // for focusing the specified area where ever required
+//  function msg(){
+//    foc.style.background="green";               // the input background area focused to green while clicking
+
+// foc.style.color="green";    }                         // the text entered is displayed in green color 
+  
+
+
+                                                //onblur
+                                                // used to blur the input area into the given color by clicking somewhere else
+
+// function msg(){
+//     foc.style.background="green";                
+//  foc.style.color="green";    }  
+
+
+
+//                                                     //onchange
+// function books(){
+//         alert("book 1");
+//     }
+                                               
+ 
+
+
+
+                                              //on resize
+                                                 //while reducing the size of the web page the  displayed 
+                                                 //height and width value will also reduce simultaneously
+
+
+
+
+//  function displaysize(){
+//     var a=window.outerWidth;                              //stores window width in a
+//     var b=window.outerHeight;                              //stores window height in b
+//     var c="Window size:width="+a+",height="+b;             //stores height and width in c
+//     document.getElementById("result").innerHTML=c;         // in the id of p tag  get and display  the c
+
+//  }  
+//  window.onresize=displaysize;                              // calling the function and apply resize event               
+                                                
+
+
