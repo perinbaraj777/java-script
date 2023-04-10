01c9c7f9cc86b348c8e615294effa92a126854e6
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-10 12:22:04 +0530

    finding the number of the days in a month  by given year even for leap year

--- fun ex mod7.js
+++ fun ex mod7.js
@@ -1,23 +1,26 @@
+
 function findDaysInaYear(month,year){
-    if(month<=12&&month<=1){
-      console.log("invalid month")
-    }
-    else if(month==2&&year%4==0){
-      
-      console.log("29 days");
+  
+       if(month==2 && year%4==0){
+       
+       console.log("29 days");
+     
+     }
+     else if(month==2 && year%4!==0){
+       console.log("28 days");
+     }
+     else if(month==4 || month==6 || month==8 || month==9 || month==11){
+       console.log("30 days");
+     
+     }
     
-    }
-    else if(month==2&&year%4!==0){
-      console.log("28 days");
-    }
-    else if(month==4&&6&&8&&9&&11){
-      console.log("30 days");
-    
-    }
-    else{
-      console.log("31 days");
-    }
-     }          
-     findDaysInaYear(5,2023);                
-    
-    
+     else if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+       console.log("31 days");
+     }
+     else{
+      console.log("invalid");
+     }
+      }          
+      findDaysInaYear(0,2024);                
+     
+                                         
