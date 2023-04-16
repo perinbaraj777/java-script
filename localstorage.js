2bf8f5a41913f641d88f759fb96e1dc0e6664198
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-16 09:37:23 +0530

    set and get item in local storage and session storage

---
+++ localstorage.js
@@ -0,0 +1,68 @@
+// var my_name=prompt("Enter your Name")
+
+
+// localStorage.setItem("user name",+my_name)
+// document.getElementById("demo").innerHTML="Ramya"
+// // console.log(my_name)
+
+
+// localStorage.setItem("user name",+ )
+// 
+
+
+//  var btn=document.querySelector("button");                   //varadhu
+//  var head1=document.querySelector("h1");
+//  function click()
+//  {
+//   var name1=prompt("Enter your name");
+//   localStorage.setItem("username",name1);
+//   head1.textContent="Welcome" +name1;
+ 
+//  }
+//  if( ! localStorage.getItem("username")){
+//   click();
+//  }
+//  else{
+//   var name1=localStorage.getItem("username");
+//   title.textContent="Welcome" +name1;
+//  }
+// // console.log("ram")
+
+
+
+                                                          // local storage
+                                          //set item
+
+// localStorage.setItem("name","surya");         //used to store the value surya in local storaget
+// localStorage.setItem("age",30)
+
+
+                          //  clear()
+
+// localStorage.clear();            //deletes all the data in the localstorage
+
+                        //getItem
+
+// // console.log(localStorage.getItem("name"));        //gets the name value  from localstorage  and displays in the console
+  
+                         //removeItem
+
+// localStorage.removeItem("name")                    //removes the name from local storage
+
+
+                                                             // session storage
+
+//sessionStorage.setItem("name","yogesh");     //yogesh will be printed in the  inspect>>application>>session storage
+
+    //  (same functions (clear(),removeItem,getItem,setItem,) can be used for this also instead of localstorage)
+
+
+
+
+
+
+
+         // store the data in local storage using a method directly and display the stored data in same page
+
+ // localStorage.setItem("name","bheem");
+//   var a=document.getElementById("demo").innerHTML=localStorage.getItem("name");                           
