d382c39ed8aa1cbd8b091c619ba5ce19c2c5a08f
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-16 09:40:22 +0530

    arrow  function

---
+++ arrowfunction.js
@@ -0,0 +1,69 @@
+                   //ARROW FUNCTION
+                          //syntax
+// let myfunctionname=(a,d)=>a+d;             //myfunctionname is the function name
+// console.log(myfunctionname(3,4));
+
+
+// hello=(a,b)=> a+b;        //if the function has only one statement we can ignre the curley brackets
+// console.log(hello(1,2))
+
+
+// hello=(val)=>"Hello "+val;          //sample coding
+// console.log(hello("universe"));  
+
+
+
+
+                                    //Arrow Function as an Expression
+
+// let age=5;
+// let welcome = (age < 18) ?
+//     ()=>console.log('baby'):           //doubt need to clear
+//    ()=> console.log('adult');
+
+// welcome();                                     
+
+
+
+                 // Example 4: Multiline Arrow Functions
+                                  //  If a function body has multiple statements, you need to put them inside curly brackets {}. For example,
+
+
+//    let add=(a,b)=>{
+//     let result=a+b;
+// return result;
+//    }     
+//    console.log(add(5,2));                        
+
+
+
+
+
+
+
+
+// Define an arrow function addition,Substraction,Multiplication,Division which accepts a number
+//  and returns number as action value.
+
+
+// let add=(a,b)=>console.log(a+b);
+// let sub=(a,b)=>console.log(a-b);
+// let mult=(a,b)=>console.log(a*b);
+// let div=(a,b)=>console.log(a/b);
+
+// add(10,3);
+// sub(5,2);
+// mult(2,5);
+// div(10,2);
+
+
+
+
+// Calculate the length of string using arrow function
+
+// let para=()=>{
+//    let content="'creating a arrow function to declare the length of the string' ";
+// length=content.length;
+// console.log("the given string is "+content +"and the string length is "+length);
+// }
+// console.log(para());
