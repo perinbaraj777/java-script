c2ae4a71590def01a7d397154ae6e380b1bdf596
perinbaraj777 <perinbaraj777@gmail.com>
2023-04-16 09:39:10 +0530

    moodle task  of string

---
+++ stringexercise.js
@@ -0,0 +1,21 @@
+
+// Get the position of c in the string txt.
+// var txt="JavaScript is client side scripting Language";
+// console.log(txt.indexOf("c"));
+
+
+
+
+
+// Implement the slice method for returning the word “apples”.
+// var a="I eat apples the whole day";
+// console.log(a.slice(5,13));
+
+
+
+
+
+
+// Implement the correct String method for replacing the word “Hello” with the word “Hi”.
+var p="Hello World";
+console.log(p.replace("Hello","hi"));
